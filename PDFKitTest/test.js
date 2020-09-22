8f50f0cb279aba1965d5ff75cee00265033f6b63
Spencer Felton <SpencerEFelton@gmail.com>
2020-09-22 14:38:09 +0100

    Initial commit using pdfkit

---
+++ PDFKitTest/test.js
@@ -0,0 +1,14 @@
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+// Create a document
+const doc = new PDFDocument;
+
+// Pipe its output somewhere, like to a file or HTTP response
+// See below for browser usage
+doc.pipe(fs.createWriteStream('output.pdf'));
+
+doc.addPage();
+
+// Finalize PDF file
+doc.end();
