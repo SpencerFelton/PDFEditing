b363384d24ef349a71b9a37b8cb774e8b75e3f63
Spencer Felton <SpencerEFelton@gmail.com>
2020-09-23 21:24:19 +0100

    Worked out coordinate system for pdf points to pixels

--- PDFKitTest/test.js
+++ PDFKitTest/test.js
@@ -1,14 +1,40 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
+px_width = 2303; // width of page in pixels
+mm_width = 195; // width of page in mm
+
+one_mm_width = px_width/mm_width; // width of one mm in pixels
+
+one_inch_width_points = 72 // width of one inch in pdf pointspoints in pdf size
+one_inch_width_mm = 25.4 // width of one inch in mm
+
+one_mm_width_points = one_inch_width_points/one_inch_width_mm; // width of one mm in points
+
+one_px_width_points = one_mm_width_points/one_mm_width; // width of one pixel in points - multiply this by pixel positions to use in coordinate system
+
+console.log(one_px_width_points);
+
+
 // Create a document
-const doc = new PDFDocument;
+const doc = new PDFDocument({size: [552.75624, 683.14968]});
+doc.font('fonts/sweetpea.ttf')
 
 // Pipe its output somewhere, like to a file or HTTP response
 // See below for browser usage
 doc.pipe(fs.createWriteStream('output.pdf'));
+//front page:
 
-doc.image('images/pscat.jpg', 50, 50, {height: 100})
+//inside page:
+
+//page 1:
+
+//page 2:
+
+//page 3:
+doc.addPage()
+doc.image('images/book1/CM - 02.png', 806, 1406, {height: 100});
+doc.text('Hello')
 
 // Finalize PDF file
 doc.end();
