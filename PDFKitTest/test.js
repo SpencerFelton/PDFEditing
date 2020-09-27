fcdad0cf80c733a4a57c651e4b1d3f389b5d22cd
Spencer Felton <SpencerEFelton@gmail.com>
2020-09-27 22:35:48 +0100

    Heap memory issues arisen

--- PDFKitTest/test.js
+++ PDFKitTest/test.js
@@ -4,7 +4,12 @@ const fs = require('fs');
 px_width = 2303; // width of page in pixels
 mm_width = 195; // width of page in mm
 
+px_height = 2846;
+mm_height = 241;
+
 one_mm_width = px_width/mm_width; // width of one mm in pixels
+
+one_mm_height = px_height/mm_height
 
 one_inch_width_points = 72 // width of one inch in pdf pointspoints in pdf size
 one_inch_width_mm = 25.4 // width of one inch in mm
@@ -12,6 +17,8 @@ one_inch_width_mm = 25.4 // width of one inch in mm
 one_mm_width_points = one_inch_width_points/one_inch_width_mm; // width of one mm in points
 
 one_px_width_points = one_mm_width_points/one_mm_width; // width of one pixel in points - multiply this by pixel positions to use in coordinate system
+
+one_px_height_points = one_mm_width_points/one_mm_height;
 
 console.log(one_px_width_points);
 
@@ -35,7 +42,9 @@ doc.pipe(fs.createWriteStream('output.pdf'));
 doc.addPage()
 var text = "Twas the night before Christmas\nAnd snug in their house\nThe little mice gathered\nAround Mummy Mouse.";
 doc.image('images/book1/CM - 02.png', 900*one_px_width_points, 1406*one_px_width_points, {height: 150}); // width is automatically scaled to height
-doc.text(text, 800*one_px_width_points, 800*one_px_width_points) // TODO: instead of manually doing the newlines, try to sort the text wrapping 
+doc.text(text, 800*one_px_width_points, 800*one_px_width_points) // TODO: instead of manually doing the newlines, try to sort the text wrapping
 
+doc.addPage()
+//doc.image('images/book1/CM - 09a.png', {height: mm_height*one_px_height_points}); // width is automatically scaled to height ----------causes js to run out of memory in the heap
 // Finalize PDF file
 doc.end();
