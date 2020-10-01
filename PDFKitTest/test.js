b8a65d8e7b8580173bbf1f87f643018d9c5c4ed1
Spencer Felton <SpencerEFelton@gmail.com>
2020-10-01 21:53:13 +0100

    Renamed obtuse variable names
    Better, more self explanatory names

--- PDFKitTest/test.js
+++ PDFKitTest/test.js
@@ -1,26 +1,25 @@
 const PDFDocument = require('pdfkit');
 const fs = require('fs');
 
-px_width = 2303; // width of page in pixels
 mm_width = 195; // width of page in mm
+px_width = 2303; // width of page in pixels
 
-px_height = 2846;
-mm_height = 241;
+mm_height = 241; // height of page in mm
+px_height = 2846; // height of page in pixels
 
-one_mm_width = px_width/mm_width; // width of one mm in pixels
+one_inch_to_points = 72 // pdf measures dimensions in points, 72 points = 1 inch
+one_inch_to_mm = 25.4 // 1 inch = 25.4 mm
 
-one_mm_height = px_height/mm_height
+one_mm_width_in_px = px_width/mm_width; // pixels per one mm horizontally
+one_mm_height_in_px = px_height/mm_height // pixels per one mm vertically
 
-one_inch_width_points = 72 // width of one inch in pdf pointspoints in pdf size
-one_inch_width_mm = 25.4 // width of one inch in mm
+one_mm_in_points = one_inch_to_points/one_inch_to_mm; // width of one mm in points (72/25.4)
 
-one_mm_width_points = one_inch_width_points/one_inch_width_mm; // width of one mm in points
+one_px_width_in_points = one_mm_in_points/one_mm_width_in_px; // width of one pixel in points - multiply this by pixel positions to use in coordinate system
 
-one_px_width_points = one_mm_width_points/one_mm_width; // width of one pixel in points - multiply this by pixel positions to use in coordinate system
+one_px_height_in_points = one_mm_in_points/one_mm_height_in_px;
 
-one_px_height_points = one_mm_width_points/one_mm_height;
-
-console.log(one_px_width_points);
+console.log(one_px_width_in_points);
 
 
 // Create a document
@@ -41,12 +40,12 @@ doc.pipe(fs.createWriteStream('output.pdf'));
 //page 3:
 doc.addPage()
 var text = "Twas the night before Christmas\nAnd snug in their house\nThe little mice gathered\nAround Mummy Mouse.";
-doc.image('images/book1/CM - 02.png', 900*one_px_width_points, 1406*one_px_width_points, {height: 150}); // width is automatically scaled to height
-doc.text(text, 800*one_px_width_points, 800*one_px_width_points) // TODO: instead of manually doing the newlines, try to sort the text wrapping
+doc.image('images/book1/CM - 02.png', 900*one_px_width_in_points, 1406*one_px_height_in_points, {height: 150}); // width is automatically scaled to height
+doc.text(text, 800*one_px_width_in_points, 800*one_px_height_in_points) // TODO: instead of manually doing the newlines, try to sort the text wrapping
 
 doc.addPage()
 var text = '"Now who can it be?"\nNAMEnamena\nCan you see who\'s there?\n\nThere\'s someone quite small\nin the cold and the snow\ncan you see who knocked\nJust a moment ago?'
-doc.image('images/book1/CM - 09a-scaled-down.png', 0, 0, {height: px_height*one_px_height_points}); // width is automatically scaled to height ---------- STILL CAUSES HEAP OUT OF MEM AFTER INCREASING MEMORY ALLOWANCE
-doc.text(text, 900*one_px_width_points, 1800*one_px_height_points) // adding text in rough area
+doc.image('images/book1/CM - 09a-scaled-down.png', 0, 0, {height: px_height*one_px_height_in_points}); // width is automatically scaled to height ---------- STILL CAUSES HEAP OUT OF MEM AFTER INCREASING MEMORY ALLOWANCE
+doc.text(text, 900*one_px_width_in_points, 1800*one_px_height_in_points) // adding text in rough area
 // Finalize PDF file
 doc.end();
