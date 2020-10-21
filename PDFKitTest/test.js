50e521408d7fbb3ddb34bb7f370650e3e990a7f5
Spencer Felton <SpencerEFelton@gmail.com>
2020-10-21 23:42:42 +0100

    Update test.js

--- PDFKitTest/test.js
+++ PDFKitTest/test.js
@@ -142,7 +142,7 @@ function buildChristmas(book_size, custom_name){
   var text = replaced_name_text[6]
   doc.image('images/FinishedImages/WOW Lost at Christmas_10.jpg', {height: calculatedDimensions[1]*px_height, width: calculatedDimensions[0]*px_width});
   doc.text(text, 500*calculatedDimensions[0], 2000*calculatedDimensions[1]);
-  //page 8:
+  //page 8: TODO: ADD TEXT TO THESE PAGES
   doc.addPage();
   doc.image('images/FinishedImages/WOW Lost at Christmas_11.jpg', {height: calculatedDimensions[1]*px_height, width: calculatedDimensions[0]*px_width});
   //page 9:
